715e2c7b52ff271ccb0cf6e246b1847ca266085f
庄园 <zhuangyuan@zhuangyuandeMacBook-Pro.local>
2017-03-15 16:33:09 -0300

    use class

--- cipher.py
+++ cipher.py
@@ -1,73 +1,65 @@
-#Caesar_cipher,Vigenere_cipher,Matrix_transposition_cipher
-#Copyright Yuan-B00755386
 import numpy
-def Caesar_encrypt(PT,Key):
-    return ''.join([chr((ord(item)+Key-65)%26+65) for item in PT])
-    # use the related askII code to calculate
-
-def Caesar_decrypt(CT,Key):
-    return ''.join([chr((ord(item)-Key-65)%26+65) for item in CT])
-
-def Vigenere_encrypt(PT,Key):
-    Gen_Key = [Key[i % len(Key)] for i in range(len(PT))]
-    # repeat letter of key to the length of plain text
-    return ''.join([chr((ord(Gen_Key[i])+ord(PT[i])-130)%26+65) for i in range(len(PT))])
-    # use askII code to calculate swift times
-
-def Vigenere_decrypt(CT,Key):
-    Gen_Key = [Key[i % len(Key)] for i in range(len(CT))]
-    return ''.join([chr((ord(CT[i]) - ord(Gen_Key[i]))%26+65) for i in range(len(CT))])
-
-def Matrix_transposition_encrypt(PT,Key):
-    gen_String = [i for i in PT.ljust(len(PT)+len(Key)-len(PT)%len(Key)).replace(' ','%')]
-    #fill space with %
-    gen_Matrix = numpy.array(gen_String).reshape(int(len(gen_String)/len(Key)),len(Key)).T
-    # map the string into matrix and return its transpose
-    return ''.join([''.join(gen_Matrix[int(i)-1]) for i in Key])
-    # get text follow the key sequence
-
-def Matrix_transposition_decrypt(CT,Key):
-    rows = int(len(CT)/len(Key))
-    CT = [CT[i:i+rows] for i in range(0,len(CT),rows)] # map string into matrx
-    sort =[Key.index(str(i+1)) for i in range(len(Key))]
-    PT = [[j for j in CT[i]] for i in sort] # resort follow the sequence of key
-    PT = numpy.array(PT).T # get PT's transpose
-    return ''.join([''.join(PT[i]) for i in range(len(PT))]).replace('%',' ').strip()
-
-if __name__ == '__main__':
-    while True :# call functions
-            n = input("Please choose a cipher method, 1 for Caesar, 2 for Vigenere, 3 for Matrix transposition, 0 to quit: ")
-            if n not in ['1','2','3','0']:
+class Cipher:
+    (text,key,n) = ('','','')
+    def __init__(self):# input number to choose a cipher methed
+        while True:
+            self.n =input("Please choose a cipher method, 1 for Caesar, 2 for Vigenere, 3 for Matrix transposition, 0 to quit:  ")
+            if self.n not in ['0','1','2','3']:
                 print('invalid input, please try again.')
                 continue
-            if n == '0':
-                break
-            while True:
-                Implement = input("Input 'E' for encryption or 'D' for decrption: ")
-                if Implement=='E':
-                    PT = input("Please input the plaintext: ")
-                    key = input("Please input the secret key: ")
-                    if n == '1':
-                        print("The ciphertext: ",Caesar_encrypt(PT.strip(),int(key)))
-                        break
-                    elif n == '2':
-                        print("The ciphertext: ",Vigenere_encrypt(PT.strip(), key.strip()))
-                        break
-                    elif n == '3':
-                        print("The ciphertext: ",Matrix_transposition_encrypt(PT.strip(), key.strip()))
-                        break #"security!security!security!", "54123"
-                elif Implement=='D':
-                    CT = input("Please input the ciphertext: ")
-                    key = input("Please input the secret key: ")
-                    if n == '1':
-                        print("The plaintext: ",Caesar_decrypt(CT.strip(), int(key)))
-                        break
-                    elif n =='2':
-                        print("The plaintext: ",Vigenere_decrypt(CT.strip(), key.strip()))
-                        break
-                    elif n == '3':
-                        print("The plaintext: ",Matrix_transposition_decrypt(CT.strip(), key.strip()))
-                        break #"rsiet%u!rsi%sietcyetcyu!cyu!r%","54123"
-                else:
+            elif self.n == '0': break
+            else: self.choose_cipher()
+
+    def choose_cipher(self):# choose to encrypt or decrupt
+        while True:
+                sign = input("Input 'E' for encryption or 'D' for decrption, '0' to quit: ")
+                dict = {'E': 'the plaintext:  ', 'D': 'the ciphertext:  ','0':''}
+                if sign not in dict:
                     print('invalid input, please try again.')
                     continue
+                if sign == '0': break
+                self.text = input("Please input %s"%dict[sign])
+                self.key = input("Please input the secret key: ")
+                if sign == 'E':
+                    if self.n == '1': print(dict['D'],self.Caesar_encrypt())
+                    elif self.n == '2': print(dict['D'],self.Vigenere_encrypt())
+                    elif self.n == '3': print(dict['D'],self.Matrix_transposition_encrypt())
+                elif sign == 'D':
+                    if self.n == '1': print(dict['E'],self.Caesar_decrypt())
+                    elif self.n == '2': print(dict['E'],self.Vigenere_decrypt())
+                    elif self.n == '3': print(dict['E'],self.Matrix_transposition_decrypt())
+
+    def Caesar_encrypt(self):
+        return  ''.join([chr((ord(item) +int(self.key) - 65) % 26 + 65) for item in self.text])
+        # use the related askII code to calculate
+    def Caesar_decrypt(self):
+        return ''.join([chr((ord(item) - int(self.key) - 65) % 26 + 65) for item in self.text])
+
+    def Vigenere_encrypt(self):
+        Gen_Key = [self.key[i % len(self.key)] for i in range(len(self.text))]
+        # repeat letter of key to the length of plain text
+        return ''.join([chr((ord(Gen_Key[i]) + ord(self.text[i]) - 130) % 26 + 65) for i in range(len(self.text))])
+        # use askII code to calculate swift times
+
+    def Vigenere_decrypt(self):
+        Gen_Key = [self.key[i % len(self.key)] for i in range(len(self.text))]
+        return ''.join([chr((ord(self.text[i]) - ord(Gen_Key[i])) % 26 + 65) for i in range(len(self.text))])
+
+    def Matrix_transposition_encrypt(self):
+        gen_String = [i for i in self.text.ljust(len(self.text) + len(self.key) - len(self.text) % len(self.key)).replace(' ', '%')]
+        # fill space with %
+        gen_Matrix = numpy.array(gen_String).reshape(int(len(gen_String) / len(self.key)), len(self.key)).T
+        # map the string into matrix and return its transpose
+        return ''.join([''.join(gen_Matrix[int(i) - 1]) for i in self.key])
+        # get text follow the key sequence
+
+    def Matrix_transposition_decrypt(self):
+        rows = int(len(self.text) / len(self.key))
+        CT = [self.text[i:i + rows] for i in range(0, len(self.text), rows)]  # map string into matrx
+        sort = [self.key.index(str(i + 1)) for i in range(len(self.key))]
+        PT = [[j for j in CT[i]] for i in sort]  # resort follow the sequence of key
+        PT = numpy.array(PT).T  # get PT's transpose
+        return ''.join([''.join(PT[i]) for i in range(len(PT))]).replace('%', ' ').strip()
+
+if __name__ == '__main__':
+    a = Cipher()
